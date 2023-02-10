c52d91442776d03749aa98021766932a7824c26e
Drynn41 <117463230+Drynn41@users.noreply.github.com>
2023-02-10 15:51:28 +0100

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,22 @@
+class minecraft {
+  file {'/opt/minecraft':
+    ensure => directory,
+  }
+  file {'/opt/minecraft/minecraft_server.jar':
+    ensure => file,
+    source => 'https://piston-data.mojang.com/v1/objects/c9df48efed58511cdd0213c56b9013a7b5c9ac1f/server.jar',
+  }
+  package { 'jdk-17':
+    ensure => present,
+    source => 'https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm',
+  }
+  file {'/opt/minecraft/eula.txt':
+    ensure => file,
+    content => 'eula=true',
+  }
+  file {'/etc/systemd/system/minecraft.service':
+    ensure => file,
+    source => 'puppet:///modules/minecraft/minecraft.service',
+  }
+  
+}
